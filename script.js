f0530e1583d7d76d02584edcdc70099d43e11169
a_osovsky <116560582+ArtemyOsovskoi@users.noreply.github.com>
2024-12-21 20:08:45 +0300

    added cath errors and promise logic for fetch

--- script.js
+++ script.js
@@ -3,53 +3,62 @@ const btn = document.getElementById("refresh_btn");
 const search = document.getElementById("search_field");
 const searchBtn = document.getElementById("search_btn");
 
-fetch(
-  `https://api.giphy.com/v1/gifs/translate?api_key=7CkGGQkqUHujXAe1i31OgekECUyWWP8O&s=cats`,
-  { mode: "cors" },
-)
-  .then(function (response) {
-    return response.json();
-  })
+const catUrl = `https://api.giphy.com/v1/gifs/translate?api_key=7CkGGQkqUHujXAe1i31OgekECUyWWP8O&s=cats`;
 
-  .then(function (response) { 
-    img.src = response.data.images.original.url;
-  });
+async function getCatData() {
+  try {
+    const response = await fetch(catUrl);
+    const catData = await response.json();
+    if (response.ok) {
+      img.src = catData.data.images.original.url;
+      console.log("Promise resolved");
+    } else {
+      console.error("Promise resolved but HTTP status failed");
+    }
+  } catch {
+    console.error("Promise rejected");
+  }
+}
+getCatData();
 
+async function fetchData() {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=7CkGGQkqUHujXAe1i31OgekECUyWWP8O&s=${search.value}`,
+      { mode: "cors" },
+    );
+    const data = await response.json();
 
-function fetchData() {
-  fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=7CkGGQkqUHujXAe1i31OgekECUyWWP8O&s=${search.value}`,
-    { mode: "cors" },
-  )
-    .then(function (response) {
-      return response.json();
-    })
-
-    .then(function (response) { 
-      img.src = response.data.images.original.url;
-    });
-};
-//fetchData();
+    if (response.ok) {
+      img.src = data.data.images.original.url;
+      console.log("Promise resolved");
+    } else {
+      console.error("Promise resolved but HTTP status failed");
+    }
+  } catch {
+    console.error("Promise rejected");
+  }
+}
 btn.addEventListener("click", fetchData);
-
 
 let userInput = search.value;
 
-
-function searchGif() {
-  fetch(
-    `https://api.giphy.com/v1/gifs/search?q=${search.value}&api_key=7CkGGQkqUHujXAe1i31OgekECUyWWP8O&limit=5`, 
-    { mode: "cors" },
-  )
-.then(function (response) {
-  return response.json();
-})
-.then(function (response) {
-  //console.log(response);
-  img.src = response.data[0].images.original.url;
-});
-};
+async function searchGif() {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?q=${search.value}&api_key=7CkGGQkqUHujXAe1i31OgekECUyWWP8O&limit=5`,
+      { mode: "cors" },
+    );
+    const data = await response.json();
+    if (response.ok) {
+      img.src = data.data[0].images.original.url;
+      console.log("Promise resolved");
+    } else {
+      console.error("Promise resolved but HTTP status failed");
+    }
+  } catch {
+    console.error("Promise rejected");
+  }
+}
 
 searchBtn.addEventListener("click", searchGif);
-
-
